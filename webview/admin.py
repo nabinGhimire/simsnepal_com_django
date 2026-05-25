f5eb180f270691017327dc9c2e5dae9aa1641067
Nabin Ghimire <nabin@hamro.com>
2026-05-25 17:32:33 +0545

    duplicate teacher admin removed

--- webview/admin.py
+++ webview/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register the Teacher model from the sms app so admins can create / edit teachers.
-from sms.models import Teacher
+# from sms.models import Teacher
 
-admin.site.register(Teacher)
+# admin.site.register(Teacher)
