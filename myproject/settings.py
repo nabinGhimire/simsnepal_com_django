7ecd55b1b27d0340f77af41baa57e737bd87dd30
Nabin Ghimire <nabin@hamro.com>
2026-05-28 02:21:22 +0545

    CSS optimized

--- myproject/settings.py
+++ myproject/settings.py
@@ -58,11 +58,12 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'sms.middleware.ThreadLocalRequestMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware',  # GZip compression
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'myproject.urls'
--- staticfiles/nepali_datetime_field/init.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('input[type=nepali-date]').nepaliDatePicker({
-        dateFormat: '%y-%m-%d',
-        closeOnDateSelect: true,
-        minDate: '१९७५-१-१',
-        maxDate: '२१००-१२-३०'
-    });
-});
